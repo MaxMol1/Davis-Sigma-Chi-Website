ad758a9a10cc3dfd4c3bd00775b2c5c452e4ce2f
Max Molchanov <mmolchanov@ucdavis.edu>
2020-02-06 17:34:44 -0800

    file name and class name changes

--- js/script.js
+++ js/script.js
@@ -22,16 +22,16 @@
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
-    target: '#mainNav',
+    target: '#navigation',
     offset: 100
   });
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
+    if ($("#navigation").offset().top > 100) {
+      $("#navigation").addClass("navbar-shrink");
     } else {
-      $("#mainNav").removeClass("navbar-shrink");
+      $("#navigation").removeClass("navbar-shrink");
     }
   };
   // Collapse now if page is not at top
